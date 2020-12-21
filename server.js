5229345f58f4a2c42aa2015d66c155fb8dfaedb5
singhalharsh10 <hsinghal1_be18@thapar.edu>
2020-12-21 13:50:57 +0530

    production build   done

--- server.js
+++ server.js
@@ -14,8 +14,7 @@
  const Emitter = require('events')
 
  // Database Connection
- const url = 'mongodb://localhost/pizza';
- mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true });
+ mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true });
  const connection = mongoose.connection;
  connection.once('open', () => {
      console.log('Database Connected..')
@@ -75,7 +74,10 @@
  app.set('view engine', 'ejs')
 
  require('./routes/web')(app)
+ app.use((req, res) => {
+     res.status(404).send('<h1>404, Page Not Found</h1>')
 
+ })
 
 
  const server = app.listen(PORT, () => {
